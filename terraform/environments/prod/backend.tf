27cafa6567a58037035c081f2551ac253f7ca81c
gsmothilal <mothilal488@gmail.com>
2025-08-08 13:11:05 +0530

    Update backend.tf

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -1,10 +1,11 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"      
-    storage_account_name = "tfstatestorageacct11"      
+    storage_account_name = "tfstatestorageaccount567"      
     container_name       = "tfstate"                 
     key                  = "functions.tfstate"  
   }
 }
+
 
 
